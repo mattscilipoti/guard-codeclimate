8b02decec21754edfff3a857fb50b5be21ac8571
Marcos Wright-Kuhns <marcoswk@haikulearning.com>
2016-03-21 10:08:27 -0700

    Add a tiny bit of verbosity

--- lib/guard/code_climate.rb
+++ lib/guard/code_climate.rb
@@ -5,23 +5,17 @@ require 'shellwords'
 
 module Guard
   class CodeClimate < Shell
-
     # Call #run_on_change for all files which match this guard.
     def run_all
       run_on_modifications(Compat.matching_files(self, Dir.glob('{,**/}*{,.*}')))
     end
 
     # Print the result of the command(s), if there are results to be printed.
-    def run_on_modifications(*files)
+    def run_on_modifications(files)
       files.each do |path|
+        puts "Performing Code Climate analysis of #{path.inspect}"
         ::Guard::Dsl.new.eager("codeclimate analyze #{Shellwords.escape(path)}")
       end
     end
   end
-
-  #class Dsl
-  #  def analyze(path)
-  #    
-  #  end
-  #end
 end
